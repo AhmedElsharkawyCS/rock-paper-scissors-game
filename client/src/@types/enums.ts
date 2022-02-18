a553548b49e18bce59f486dcb908fa6f9fd054fa
Ahmed Elsharkawy <ahmedelsharkawy.fcih@gmail.com>
2022-02-18 06:51:30 +0200

    added types

---
+++ client/src/@types/enums.ts
@@ -0,0 +1,27 @@
+export enum RPSGameValuesOptions {
+  ROCK = "rock",
+  PAPER = "paper",
+  SCISSORS = "scissors",
+}
+
+export enum RPSGameModeOptions {
+  PLAYER_VS_COMPUTER = "player vs computer",
+  COMPUTER_VS_COMPUTER = "computer vs computer",
+}
+
+export enum GameEvents {
+  INIT_GAME = "init:game",
+  GAME_INITIATED = "game:initiated",
+  JOIN_GAME = "join:game",
+  PLAYER1_JOINED = "player:one:joined",
+  PLAYER2_JOINED = "player:two:joined",
+  PLAYER1_ANSWER = "player:one:answer",
+  PLAYER2_ANSWER = "player:two:answer",
+  GAME_RESULT = "game:result",
+  GAME_ERROR = "game:error",
+}
+
+export enum GameErrorCodes {
+  INVALID_DATA = "Invalid_data",
+  INTERNAL_ERROR = "internal_error",
+}
---
+++ client/src/@types/interfaces.ts
@@ -0,0 +1,32 @@
+import { RPSGameValuesOptions, RPSGameModeOptions } from "./enums"
+
+export interface IPlayer {
+  name: string
+  value: RPSGameValuesOptions
+}
+
+export interface IPlayerGameResult {
+  name: string | "No winner"
+  value: RPSGameValuesOptions | "tie"
+}
+export interface IGamePlayer {
+  createdAt: number
+  creator: IPlayer
+  secondPlayer?: IPlayer
+}
+export interface IGamePlayers {
+  [key: string]: IGamePlayer
+}
+
+export interface IGameRoom {
+  roomId: string
+  gameInfo: IGamePlayer
+  mode?: RPSGameModeOptions
+}
+
+export interface ISocketPayload {
+  playerName?: string
+  mode?: RPSGameModeOptions
+  roomId?: RPSGameModeOptions
+  answer?: RPSGameValuesOptions
+}
